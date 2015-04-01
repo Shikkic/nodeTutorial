36aa552706c09aed7cba2d833d28631af11dfc83
Shikkic <Shikkic@gmail.com>
2015-04-01 07:41:09 -0400

    awesome

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('./public'));
 /*
 app.post('/api/test', function(req, res) {
@@ -17,7 +17,7 @@ app.post('/api/test', function(req, res) {
 });
 */
 app.get('/', function(req, res) {
-	res.sendfile('./index.html');
+	res.sendfile('./public/index.html');
 });
 
 app.listen(app.get('port'), function() {
