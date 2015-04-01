3336258bbb50d222edafe261d570c7ee059d971f
Shikkic <Shikkic@gmail.com>
2015-04-01 08:22:37 -0400

    if this doesn't work..

--- server.js
+++ server.js
@@ -3,21 +3,13 @@ var app = express();
 var request = require('request');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
-/*
-app.post('/api/test', function(req, res) {
-	req.on('data', function(data) {
-		var bodyObj = JSON.parse(data);
-		var name = bodyObj.name;
-		console.log(name);
-	});
-	req.on('end', function() {
-		console.log("Completed.");
-	});
-	res.status(200).send({status: "Recieved, bruh"});
-});
-*/
+
 app.get('/', function(req, res) {
 	res.sendfile('./public/index.html');
+});
+
+app.get('/storytime', function(req, res) {
+	res.status(200).send({status: "I have recieved your msg "+req.query.name});
 });
 
 app.listen(app.get('port'), function() {
