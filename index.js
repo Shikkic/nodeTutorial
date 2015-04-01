f076074a5f1396b00fd4b6167fe7d94748da9065
Shikkic <Shikkic@gmail.com>
2015-03-31 21:11:27 -0400

    lol

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 /*
 app.post('/api/test', function(req, res) {
@@ -20,6 +20,6 @@ app.get('/', function(req, res) {
 	res.sendfile('./index.html');
 });
 
-app.listen(5000, function() {
+app.listen(app.get('port'), function() {
 	console.log('Fuck it ship it.');
 });
