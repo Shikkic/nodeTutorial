7005cfc652a11b9b07fb563f6615a2e19276b631
Shikkic <Shikkic@gmail.com>
2015-04-01 07:45:23 -0400

    awesome

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 /*
 app.post('/api/test', function(req, res) {
